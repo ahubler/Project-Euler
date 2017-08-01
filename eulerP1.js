ee92d886e48522b62078035bcc4c50e1583cec2d
Andrew Hubler <hubler.andrew@gmail.com>
2017-07-31 23:12:23 -0400

    updated eulerP1.js with code to complete probelm 1 of the Euler project

--- eulerP1.js
+++ eulerP1.js
@@ -1,7 +1,12 @@
+//Problem 1 asks for the sum of all multiples of 3 or 5 up to 1000
 function main(){
-  $('.output').append('<p>One</p>');
-  $('.output').append('<p>Two</p>');
-  $('.output').append('<p>One</p>');
+  var mySum = 0;//will keep the running total sum
+  var upperLimit = 1000;//we will check numbers below this value
+  for (var i = 1; i< upperLimit; i++){
+    if (i%3 === 0||i%5===0){//if i is multiple of 3 or 5
+      mySum+=i;
+    }
+  }
+  $('.output').append('<p>'+mySum+'</p>');//outputs the sum of all mutiples
 }
-
 $(document).ready(main)
